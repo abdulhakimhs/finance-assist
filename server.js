cef7935a9f9c2d16c1db82077b5480db1d44cc4a
abdulhakimhs <abdul.hakim.pekl@gmail.com>
2022-06-11 13:50:45 +0700

    Fixing typo NODE_ENV

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api/transaction/', transactionsRoute)
 
 const port = process.env.PORT || 5000
 
-if (process.env.MODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname), 'client/build/index.html')
